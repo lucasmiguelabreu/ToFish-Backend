85e8b7f90d62eb1a511a07151761f8e87aeeb754
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-05-02 16:11:43 -0300

    configurações de banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,13 @@
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    usename: 'postgres',
-    password: 'postgres',
-    database: 'devburger',
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-    },
-
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  usename: 'postgres',
+  password: 'postgres',
+  database: 'devburger',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
 };
