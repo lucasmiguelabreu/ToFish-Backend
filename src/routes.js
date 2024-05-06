bb5cb50ceba20048e24d6b61bb564141922df08c
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-05-06 13:49:28 -0300

    alterações

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import UserContrller from './app/controllers/UserController';
+import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProductController from './app/controllers/ProductController';
 
 const routes = new Router();
 
-routes.post('/users', UserContrller.store);
+routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.post('/products', ProductController.store);
+
 
 export default routes;
