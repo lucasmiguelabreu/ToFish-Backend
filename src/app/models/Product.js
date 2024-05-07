b5fcb0ed336cdba83068484183e487fdaaac74ca
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-05-07 13:48:22 -0300

    Alterações

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -19,6 +19,8 @@ class Product extends Model {
                 sequelize,
             }
         );
+
+        return this;
     }
 }
 
---
+++ src/database/migrations/20240507130208-add-category-id-column.js
@@ -0,0 +1,22 @@
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('products', 'category_id', { 
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+     });
+    
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('products', 'category_id');
+  }
+};
