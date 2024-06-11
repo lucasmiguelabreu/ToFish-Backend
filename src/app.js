c685073aeec3e010a290249b941267ab5ad038b9
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-06-11 16:12:31 -0300

    manutenções em construção

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { resolve } from 'node:path';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
@@ -8,6 +9,7 @@ class App {
     constructor() {
         this.app = express()
 
+        this.app.use(cors());
         this.middlewares()
 
         this.routes()
